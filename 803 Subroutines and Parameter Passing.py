10232b306b8924a8c736f457e50b88b90521f66e
Bennet <ybennetbthomas@gmail.com>
2025-10-29 11:22:17 +0000

    Tasks 1-3

--- 803 Subroutines and Parameter Passing.py
+++ 803 Subroutines and Parameter Passing.py
@@ -1,5 +1,5 @@
 #modular programming
-
+"""
 def displayNames(nameList):
   print("Current list")
   for loop in range(len(nameList)):
@@ -14,3 +14,45 @@ for names in range(numNames):
   nameList.append([newForename, newSurname])
 
 displayNames(nameList)
+
+# Predict and Run - Task 2
+
+def displayScoreData(scores):
+  average = sum(scores)/len(scores)
+  print("Average score:",round(average,1))
+  print("Scores ranged from:")
+  print(min(scores),"to",max(scores))
+
+# Main program
+scores = [4,6,8,5,6,3,5,9,10,2,4,6,3,5]
+displayScoreData(scores)
+scores = [50,51,52,56,59,68]
+displayScoreData(scores)
+
+"""
+
+def module1(first, second, third):
+  volume = first * second * third
+  print("Volume =",volume)
+
+def module2(monday):
+  monday = monday/10
+  return monday
+
+def module3(blue, red):
+  total = 0
+  for counter in range(blue, red):
+    total = total + 2
+    print(total)
+    return total
+
+def module4(up, down):
+  middle = up % down
+  top = int(up/down)
+  total = middle + top
+  return total
+
+num1 = 100
+num2 = 6
+num3 = 9
+print(module2(num1) + module3(num2, num3))
