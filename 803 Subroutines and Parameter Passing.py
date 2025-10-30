91df8b11862a25ad3a9dd18f41f8fec9d5e39d54
Bennet <ybennetbthomas@gmail.com>
2025-10-30 09:29:33 +0000

    Task 2

--- 803 Subroutines and Parameter Passing.py
+++ 803 Subroutines and Parameter Passing.py
@@ -30,7 +30,7 @@ scores = [50,51,52,56,59,68]
 displayScoreData(scores)
 
 """
-
+'''
 def module1(first, second, third):
   volume = first * second * third
   print("Volume =",volume)
@@ -40,11 +40,11 @@ def module2(monday):
   return monday
 
 def module3(blue, red):
-  total = 0
-  for counter in range(blue, red):
-    total = total + 2
-    print(total)
-    return total
+    total = 0
+    for counter in range(blue, red):
+        total = total + 2
+        print(total)
+        return total
 
 def module4(up, down):
   middle = up % down
@@ -56,3 +56,4 @@ num1 = 100
 num2 = 6
 num3 = 9
 print(module2(num1) + module3(num2, num3))
+'''
---
+++ 805 Linear Search & Counting Occurrences.py
@@ -0,0 +1,67 @@
+'''
+names = ['Jim', 'James', 'Jan', 'Jimmy', 'John', 'Jenna', 'Jonny' ]
+counter = 0
+nameToFind = 'John'
+found = False
+foundPosition = -1
+while counter < len(names) and found == False:
+  if names[counter] == nameToFind:
+    found = True
+    foundPosition = counter
+  else:
+    counter = counter + 1
+
+if found == True:
+  print (nameToFind, " was found at position: ", foundPosition)
+else:
+  print (nameToFind, " wasn't found. Too bad.")'
+
+teams = ["Man Utd", "Man City", "Spurs", "Liverpool", "Chelsea", "Arsenal", "Newcastle"]
+goalsScored = [63, 89, 65, 79, 59, 69, 35 ]
+
+occurrences = 0
+#Loop through every team
+for i in range(len(teams)):
+  if goalsScored[i] < 60:
+    occurrences = occurrences + 1
+    print (teams[i] +  " scored more than 60 goals")
+
+#once the loop has finished, print the total number of teams that has scored more than 60
+print (str(occurrences) + " teams have scored more than 60")
+'''
+
+# 805Task3.py
+# Standard Algorithms - Linear Search & Counting Occurrences
+
+# Investigate and modify
+
+def getTargetCharacter():
+  target = input("Enter the character you are looking for")
+
+  return target
+
+def getCharacters():
+  characters = ["Desperate Dan", "Numbskulls", "Dennis the Menace", "Minnie the Minx", "Walter", "Gnasher", "Billy Whizz"]
+
+  return characters
+
+def findCharacterPosition(oneToFind):
+  found = False
+  foundPosition = -1
+  for i in range(len(characters)):
+    if characters[i] == oneToFind:
+      found = True
+      foundPosition = i
+
+  if found == False:
+    print("The character was not found.")
+  else:
+    print("The character was found at position:", foundPosition)
+
+  return foundPosition
+
+#. *************MAIN*************
+
+target = getTargetCharacter()
+characters = getCharacters()
+foundPosition = findCharacterPosition(target)
