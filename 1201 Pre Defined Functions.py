92c67bd3441b98e1485585b9d548892bf0a46ca4
agentpython13 <153403315+agentpython13@users.noreply.github.com>
2025-11-04 13:05:04 +0000

    Update 1201 Pre Defined Functions.py

--- 1201 Pre Defined Functions.py
+++ 1201 Pre Defined Functions.py
@@ -1,3 +1,4 @@
+'''
 password = input('Enter your password:  ')
 total = 0
 for counter in range(0, len(password)):
@@ -5,4 +6,11 @@ for counter in range(0, len(password)):
 check = total % 11
 newPassword = password + str(check)
 print(newPassword)
-                
+'''
+animal = input('Enter the name of an animal in lowercase:  ')
+letter = animal[0]
+newLetter = chr(ord(animal[0])-32)
+newAnimal = newLetter + animal[1:]
+print(newAnimal)
+
+
