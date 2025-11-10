5c7a22c0e83190ebed62598d688b368464a3a729
Bennet <bennetbthomas@gmail.com>
2025-11-10 22:36:53 +0000

    Task 4

--- 1101 File Handling.py
+++ 1101 File Handling.py
@@ -5,18 +5,28 @@
 #     writefile.write(" October")
 
 #Task 3
-items = []
-names = []
-marks = []
-with open("pupils.txt") as readfile:
-    line = readfile.readline().rstrip('\n')
-    while line:
-        items = line.split(",")
-        names.append(items[0])
-        marks.append(items[1])
-        line = readfile.readline().rstrip('\n')
+# items = []
+# names = []
+# marks = []
+# with open("pupils.txt") as readfile:
+#     line = readfile.readline().rstrip('\n')
+#     while line:
+#         items = line.split(",")
+#         names.append(items[0])
+#         marks.append(items[1])
+#         line = readfile.readline().rstrip('\n')
 
-for index in range(len(names)):
-    print(names[index], '-', marks[index])
+# for index in range(len(names)):
+#     print(names[index], '-', marks[index])
+
+#Task 4
+
+names = ["John","Joan","Mark","Michael"]
+birthMonth = ['January', 'February', 'March', 'April']
+ages = [23,35,23,8]
+
+with open("names.txt","w") as wfile:
+    for counter in range(0,len(names)):
+        wfile.write(names[counter] + "," + str(ages[counter])+"\n")
 
 
