4923c70c4e85ccf409834e9154f66ec0d9dab29b
Bennet <bennetbthomas@gmail.com>
2025-11-10 22:49:21 +0000

    Challenge 4

--- 1101 File Handling.py
+++ 1101 File Handling.py
@@ -1,3 +1,4 @@
+import random
 # Task 2
 # with open("newfile.txt","w") as writefile:
 #     writefile.write("Bennet Thomas,")
@@ -20,13 +21,25 @@
 #     print(names[index], '-', marks[index])
 
 #Task 4
+# names = ["John","Joan","Mark","Michael"]
+# birthMonth = ['January', 'February', 'March', 'April']
+# ages = [23,35,23,8]
 
-names = ["John","Joan","Mark","Michael"]
-birthMonth = ['January', 'February', 'March', 'April']
-ages = [23,35,23,8]
+# with open("names.txt","w") as wfile:
+#     for counter in range(0,len(names)):
+#         wfile.write(names[counter] + "," + birthMonth[counter] + ',' + str(ages[counter])+"\n")
 
-with open("names.txt","w") as wfile:
-    for counter in range(0,len(names)):
-        wfile.write(names[counter] + "," + birthMonth[counter] + ',' + str(ages[counter])+"\n")
+#Challenge 1
+counter = 0
+with open('randomnumber.txt', 'w') as file:
+    for number in range(100):
+        file.write(str(random.randint(1,100))+'\n')
 
-
+with open('randomnumber.txt', 'r') as file:
+    number = file.readline().rstrip('\n')
+    while number:
+        if number[0] == 13:
+            counter += 1
+print('The number 13 occurs', counter, 'time(s) in the list.')
+        
+        
