1246a0734df918ac6b8e01f82a8c546064d72833
Bennet <ybennetbthomas@gmail.com>
2025-11-21 14:40:45 +0000

    Task 1

---
+++ 1101 File Handling.py
@@ -0,0 +1,6 @@
+# with open("SampleFile.txt") as readfile:
+#     filecontents = readfile.read()
+#     print(filecontents)
+
+with open('antonstinks.txt', 'w') as writefile:
+    writefile.write('Anton does actually stink. /n')
